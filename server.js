1288da31448deb2d4187e02e862ab44e2dd68c16
unknown <varishyadav@gmail.com>
2018-08-21 13:36:16 +0400

    setup heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.port || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname+ '/views/partials')
@@ -36,6 +37,6 @@ app.get('/about',(req, res)=>{
         name: 'Harish Yadav',
     });
 });
-app.listen(3000,()=>{
-    console.log('server is up on port 3000')
+app.listen(port,()=>{
+    console.log(`server is up on port ${port}`);
 });
